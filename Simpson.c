7d4259a23619adc1d355969680f2456fbf5a5eb1
Pszemyslav <pszemyslav001@gmail.com>
2015-02-01 21:24:40 +0100

    Poprawna metoda simpsona
    Poprawny wzór do obliczania całki

--- Simpson.c
+++ Simpson.c
@@ -3,31 +3,30 @@
 #include <math.h>
 #include <string.h>
 
+
 double f(double x)
 {
 	return 4*x*x*x - 3*x*x + 6*x - 10;
-	}    /*dana funkcja podlegajaca calkowaniu. */
-double calka_ozn(double x, double a, double b, double c)
-{
-	return a*x*x*x/3 + b*x*x/2 + c*x;
-	}  /*funcja pomagająca przy obliczaniu całki oznaczonej*/
+	}    /*funkcja podlegajaca calkowaniu. */
+
+
 int main(int argc, char *argv[]) /*(a,b)-przedział, n-liczba podprzdzialow na które dzielimy przedzial */
 {       
 	double a= argc >1 ? atof(argv[1]) : 0;
 	double b= argc >2 ? atof(argv[2]) : 10;
 	int n =argc >3 ? atoi(argv[3]) : 100;
 	int i;
-	double h=(b-a)/(n+1);
+
+	double h=(b-a)/n;
 	double calka=0;
+
 	for(i=0;i<n;i++)
 	{	
 		double x1 = a+i*h;	/*współrzędne podprzedzialow*/
 		double x2 = a+(i+1)*h;
 		double sr = (x1+x2)/2;		/*srodek przedzialu*/
-		double wa = (f(x2)-f(x1) - (f(sr)-f(x1)) * (x2-x1)/(sr-x1)) / (x2*x2-x1*x1 - (sr*sr-x1*x1) * (x2-x1)/(sr-x1));
-		double wb = (f(sr)-f(x1) - wa*(sr*sr-x1*x1)) / sr-x1;
-		double wc = f(x1) - wa*x1*x1 - wb*x1;			/*wa, wb, wc - współczynniki wzoru paraboli*/
-		calka = calka + calka_ozn(x2, wa, wb, wc) - calka_ozn(x1, wa, wb, wc);	/*oblicznie całki oznaczonej dla konkretnych przedziałow*/
+
+		calka = calka + h*( f(x1) + 4*f(sr) + f(x2) )/6;  /*W tym programie "h" jest to szerokość całego podprzedziału, a nie jego połowa. Stąd we wzorze obok dzielę przez 6,a nie przez 3*/
 		}
 	printf("%g\n",calka);
 	return 0;
